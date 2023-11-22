c5b36368a3e9ab6b0951f245b0e4cab8e60abf2b
WolfmosRioja <sergiolmos_@hotmail.com>
2023-11-22 18:15:05 +0100

    Update input.js

--- input.js
+++ input.js
@@ -7,7 +7,7 @@ export class InputHandler {
                     e.key == 'ArrowUp' || 
                     e.key == 'ArrowLeft' || 
                     e.key == 'ArrowRight' || 
-                    e.key == 'Space') 
+                    e.key == 'v') 
                 && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
             } else if (e.key === 'd') this.game.debug = !this.game.debug;
@@ -17,7 +17,7 @@ export class InputHandler {
                     e.key == 'ArrowUp' || 
                     e.key == 'ArrowLeft' || 
                     e.key == 'ArrowRight' || 
-                    e.key == 'Space'){
+                    e.key == 'v'){
                 this.keys.splice(this.keys.indexOf(e.key), 1);
             }
         });
